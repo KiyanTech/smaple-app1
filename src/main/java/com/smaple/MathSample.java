30764a7606734973ea46e66b675a72426e031f4b
susantad <babaitech01@gmail.com>
2024-03-01 18:31:36 +0530

    offline

--- src/main/java/com/smaple/MathSample.java
+++ src/main/java/com/smaple/MathSample.java
@@ -10,8 +10,9 @@ public class MathSample {
 	public static int subdev1(int a,int b) {
 		if(a>b) {
 			return a-b;
+		}else {
+			return b-a;
 		}
-		return b-a;
 	}
 	public static void main(String ...arg) {
 		
